0e38f190d86c2d5a822ce1eb968e655079d55fed
kiphaar <dewish213@gmail.com>
2025-11-27 09:27:10 +0100

    .

---
+++ opdracht 1/main.js
@@ -0,0 +1,5 @@
+
+  document.getElementById("titel").style.color = "red";
+  document.getElementById("titel").innerHTML = "doei";    
+  
+ alert("een ID kan niet 2 keer gebruikt worden, omdat het een ID iets unieks is.");
