8ac3301566ad85aa31b484688072d7be185eb0f7
kiphaar <dewish213@gmail.com>
2025-11-28 11:14:57 +0100

    .

---
+++ opdracht 5/script.js
@@ -0,0 +1,8 @@
+const getal = "20"
+const nummer = 10
+
+console.log(getal + nummer);
+document.getElementById("getal").innerHTML = getal + nummer
+
+console.log(nummer+nummer);
+document.getElementById("nummer").innerHTML = nummer + nummer
