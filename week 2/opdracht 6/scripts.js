1fe805a96c9fc40311a8ed3dad223fcaf8ccc1ac
kiphaar <dewish213@gmail.com>
2025-12-02 10:31:01 +0100

    .

---
+++ week 2/opdracht 6/scripts.js
@@ -0,0 +1,9 @@
+
+
+function kleur_aanpassen() {
+    document.getElementById("title").style.color = "red"
+    document.getElementById("title").textContent = "Inhoud aangepast!"
+
+
+}
+kleur_aanpassen();
