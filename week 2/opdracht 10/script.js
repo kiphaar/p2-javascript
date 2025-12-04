2236cad5c9e583e635a7b8422c4f89d451a404e6
kiphaar <dewish213@gmail.com>
2025-12-04 10:10:48 +0100

    .

---
+++ week 2/opdracht 10/script.js
@@ -0,0 +1,18 @@
+function groet_ochtend() {
+
+ document.getElementById("hoi").innerHTML= "Goedemorgen!"
+
+
+}
+function groet_middag() {
+
+ document.getElementById("hoi").innerHTML= "Goedemiddag!"
+
+
+}
+function groet_avond() {
+
+ document.getElementById("hoi").innerHTML= "Goedenavond!"
+
+
+}
