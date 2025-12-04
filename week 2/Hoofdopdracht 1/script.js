ec18909431f0b537d8ea4d22cf1ab0f5d81794a8
kiphaar <dewish213@gmail.com>
2025-12-04 10:46:39 +0100

    .

---
+++ week 2/Hoofdopdracht 1/script.js
@@ -0,0 +1,15 @@
+function dark(){
+
+document.body.style.backgroundColor= "black"
+document.body.style.color= "white"
+document.getElementById("text").innerHTML="slaaplekker!🌆"
+
+}
+
+function light(){
+
+document.body.style.backgroundColor= "white"
+document.body.style.color= "black"
+document.getElementById("text").innerHTML="wakey wakey!🌅"
+
+}
