b1e47c4ef9d92cd44fd855f08a42372ff5cbacee
kiphaar <dewish213@gmail.com>
2026-01-22 09:49:26 +0100

    .

---
+++ week 4/opdracht 17/script.js
@@ -0,0 +1,11 @@
+let container = document.getElementById("container");
+
+addEventListener("mouseover", function() {
+    container.style.color = "red";
+    
+});
+
+addEventListener("mouseout", function() {
+    container.style.color = "black";
+});
+
